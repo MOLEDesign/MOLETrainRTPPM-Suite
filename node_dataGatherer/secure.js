5d98e441a1e2d83621fe2504f1bfc540cd7c19fe
MOLEDesign <morgan@moledesign.biz>
2013-04-11 19:58:29 +0100

    Added variables for MYSQL Database

--- node_dataGatherer/secure.js
+++ node_dataGatherer/secure.js
@@ -2,3 +2,8 @@
 
 var username = '(username)';
 var password = '(password)';
+
+var database_host ='(database host)';
+var database_name ='(database name)';
+var database_username ='(database username)';
+var database_password ='(database password)';
